3a6bf8a9b4d4553657b983d11f1ffbd332a30ede
rkarthick2006 <rkarthick2006@gmail.com>
2022-12-14 17:16:56 +0530

    Add files via upload

---
+++ Advanced-Data-Engineering-with-Db/Includes/Classroom-Setup-1.6.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %run ./_databricks-academy-helper $lesson="1.6"
+
+# COMMAND ----------
+
+# MAGIC %run ./_utility-functions
+
+# COMMAND ----------
+
+DA.cleanup()
+DA.init()
+DA.conclude_setup()
